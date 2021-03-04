585d32d38e5a0531630ac8fc5e1c566174743027
Sebastian Schmidt <mrschmidt@google.com>
2021-03-03 21:37:10 -0700

    Cleanup

--- Firestore/core/test/unit/model/values_test.cc
+++ Firestore/core/test/unit/model/values_test.cc
@@ -122,6 +122,13 @@ class ValuesTest : public ::testing::Test {
     }
   }
 
+  template <typename... Args>
+  void Add(std::vector<std::vector<google_firestore_v1_Value>>& groups,
+           Args... values) {
+    std::vector<google_firestore_v1_Value> group{(values)...};
+    groups.emplace_back(group);
+  }
+
  private:
   remote::Serializer serializer;
 };
@@ -129,75 +136,53 @@ class ValuesTest : public ::testing::Test {
 TEST_F(ValuesTest, Equality) {
   std::vector<std::vector<google_firestore_v1_Value>> equals_group;
 
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(nullptr), Wrap(nullptr)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(false), Wrap(false)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(true), Wrap(true)});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      Wrap(std::numeric_limits<double>::quiet_NaN()),
+  Add(equals_group, Wrap(nullptr), Wrap(nullptr));
+  Add(equals_group, Wrap(false), Wrap(false));
+  Add(equals_group, Wrap(true), Wrap(true));
+  Add(equals_group, Wrap(std::numeric_limits<double>::quiet_NaN()),
       Wrap(ToDouble(kCanonicalNanBits)), Wrap(ToDouble(kAlternateNanBits)),
-      Wrap(std::nan("1")), Wrap(std::nan("2"))});
+      Wrap(std::nan("1")), Wrap(std::nan("2")));
   // -0.0 and 0.0 compare the same but are not equal.
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{Wrap(-0.0)});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{Wrap(0.0)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(1), Wrap(1LL)});
+  Add(equals_group, Wrap(-0.0));
+  Add(equals_group, Wrap(0.0));
+  Add(equals_group, Wrap(1), Wrap(1LL));
   // Doubles and Longs aren't equal (even though they compare same).
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(1.0), Wrap(1.0)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(1.1), Wrap(1.1)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(BlobValue(0, 1, 1))});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(BlobValue(0, 1))});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap("string"), Wrap("string")});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap("strin")});
+  Add(equals_group, Wrap(1.0), Wrap(1.0));
+  Add(equals_group, Wrap(1.1), Wrap(1.1));
+  Add(equals_group, Wrap(BlobValue(0, 1, 1)));
+  Add(equals_group, Wrap(BlobValue(0, 1)));
+  Add(equals_group, Wrap("string"), Wrap("string"));
+  Add(equals_group, Wrap("strin"));
   // latin small letter e + combining acute accent
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap("e\u0301b")});
+  Add(equals_group, Wrap("e\u0301b"));
   // latin small letter e with acute accent
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap("\u00e9a")});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      Wrap(Timestamp::FromTimePoint(kDate1)), Wrap(kTimestamp1)});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      Wrap(Timestamp::FromTimePoint(kDate2)), Wrap(kTimestamp2)});
+  Add(equals_group, Wrap("\u00e9a"));
+  Add(equals_group, Wrap(Timestamp::FromTimePoint(kDate1)), Wrap(kTimestamp1));
+  Add(equals_group, Wrap(Timestamp::FromTimePoint(kDate2)), Wrap(kTimestamp2));
   // NOTE: ServerTimestampValues can't be parsed via Wrap().
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
+  Add(equals_group,
       WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp1)),
-      WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp1))});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp2))});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      Wrap(GeoPoint(0, 1)), Wrap(GeoPoint(0, 1))});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(GeoPoint(1, 0))});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      WrapReference(DbId(), Key("coll/doc1")),
-      WrapReference(DbId(), Key("coll/doc1"))});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      WrapReference(DbId(), Key("coll/doc2"))});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      WrapReference(model::DatabaseId("project", "baz"), Key("coll/doc2"))});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      WrapArray("foo", "bar"), WrapArray("foo", "bar")});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{WrapArray("foo", "bar", "baz")});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{WrapArray("foo")});
-  equals_group.emplace_back(std::vector<google_firestore_v1_Value>{
-      WrapObject("bar", 1, "foo", 2), WrapObject("foo", 2, "bar", 1)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{WrapObject("bar", 2, "foo", 1)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{WrapObject("bar", 1)});
-  equals_group.emplace_back(
-      std::vector<google_firestore_v1_Value>{WrapObject("foo", 1)});
+      WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp1)));
+  Add(equals_group,
+      WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp2)));
+  Add(equals_group, Wrap(GeoPoint(0, 1)), Wrap(GeoPoint(0, 1)));
+  Add(equals_group, Wrap(GeoPoint(1, 0)));
+  Add(equals_group, WrapReference(DbId(), Key("coll/doc1")),
+      WrapReference(DbId(), Key("coll/doc1")));
+  Add(equals_group, WrapReference(DbId(), Key("coll/doc2")));
+  Add(equals_group,
+      WrapReference(model::DatabaseId("project", "baz"), Key("coll/doc2")));
+  Add(equals_group, WrapArray("foo", "bar"), WrapArray("foo", "bar"));
+  Add(equals_group, WrapArray("foo", "bar", "baz"));
+  Add(equals_group, WrapArray("foo"));
+  Add(equals_group, 
+      WrapObject("bar", 1, "foo", 2), WrapObject("foo", 2, "bar", 1);
+  Add(equals_group, 
+      WrapObject("bar", 2, "foo", 1));
+  Add(equals_group, 
+      WrapObject("bar", 1));
+  Add(equals_group, 
+      WrapObject("foo", 1));
 
   for (size_t i = 0; i < equals_group.size(); ++i) {
     for (size_t j = i; j < equals_group.size(); ++j) {
@@ -214,34 +199,34 @@ TEST_F(ValuesTest, Ordering) {
   std::vector<std::vector<google_firestore_v1_Value>> comparison_groups;
 
   // null first
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(nullptr)});
+  Add(comparison_groups, Wrap(nullptr));
 
   // booleans
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(false)});
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(true)});
+  Add(comparison_groups, Wrap(false));
+  Add(comparison_groups, Wrap(true));
 
   // numbers
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(-1e20)});
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(LLONG_MIN)});
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(-0.1)});
+  Add(comparison_groups, Wrap(-1e20));
+  Add(comparison_groups, Wrap(LLONG_MIN));
+  Add(comparison_groups, Wrap(-0.1));
   // Zeros all compare the same.
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(-0.0), Wrap(0.0), Wrap(0L)});
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(0.1)});
+  Add(comparison_groups, Wrap(-0.0), Wrap(0.0), Wrap(0L));
+  Add(comparison_groups, Wrap(0.1));
   // Doubles and longs Compare() the same.
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(1.0), Wrap(1L)});
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(LLONG_MAX)});
-  comparison_groups.emplace_back(
-      std::vector<google_firestore_v1_Value>{Wrap(1e20)});
+  Add(comparison_groups, Wrap(1.0), Wrap(1L));
+  Add(comparison_groups, Wrap(LLONG_MAX));
+  Add(comparison_groups, Wrap(1e20));
+
+  // dates
+  Add(comparison_groups, Wrap(kTimestamp1));
+  Add(comparison_groups, Wrap(kTimestamp2));
+
+  // server timestamps come after all concrete timestamps.
+  // NOTE: server timestamps can't be parsed with wrap().
+  Add(comparison_groups,
+      WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp1)));
+  Add(comparison_groups,
+      WrapServerTimestamp(FieldValue::FromServerTimestamp(kTimestamp2)));
 
   for (size_t i = 0; i < comparison_groups.size(); ++i) {
     for (size_t j = i; j < comparison_groups.size(); ++j) {
